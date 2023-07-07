be63ea296e1db5a630a084d49658ded679f03a62
Amlan Jyoti Lahon <122851257+amlanlahon@users.noreply.github.com>
2023-07-07 11:17:52 +0530

    Update Removal of Non-annotated DEGs.R

--- Removal of Non-annotated DEGs.R
+++ Removal of Non-annotated DEGs.R
@@ -1,7 +1,7 @@
 annot_genes = read.csv("annotated_genes.csv", row.names = 1)
 degs = read.csv("expression_values_of_degs_logFC1.csv", row.names = 1)
 write.csv(degs[match(rownames(annot_genes), rownames(degs)),], "annotated_DEGs_with_NA.csv")
-Degs_NA = read.csv("annotated_DEGs_with_NA.csv", row.names = 1)
+Degs_NA = read.csv("annotated_DEGs_with_NA.csv")
 annot_degs = na.omit(Degs_NA)
 write.csv(annot_degs, "annotated_degs.csv")
 annot_degs = read.csv("annotated_degs.csv")
