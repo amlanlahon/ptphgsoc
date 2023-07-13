0433b77a23fe32ff52a4d1e755766718be6b7bb6
Amlan Jyoti Lahon <122851257+amlanlahon@users.noreply.github.com>
2023-07-13 08:36:09 +0530

    Update volcano_plot.R

--- volcano_plot.R
+++ volcano_plot.R
@@ -1,3 +1,5 @@
+library(scales)0000000000
+
 vol_data_logFC1 = read.csv("volcano_data_logFC1.csv")
 
 with(vol_data_logFC1, plot(-1*logFC, -log10(adj.P.Val), pch=20, main="Volcano plot", col = alpha("black", 0.2), xlim=c(-6.2,7), xlab = "logFC", ylab = "-log10(pVal)"))
